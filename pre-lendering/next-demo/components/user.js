73b6707249edcb13e741d262792a283553fc5111
leesangyoon <zaar625@gmail.com>
2022-07-30 22:56:15 +0900

    next.js study

---
+++ pre-lendering/next-demo/components/user.js
@@ -0,0 +1,9 @@
+function User({user}){
+    return(
+        <div>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+        </div>
+    )
+}
+export default User
---
+++ pre-lendering/next-demo/pages/posts/[postId].js
@@ -0,0 +1,63 @@
+import {useRouter} from 'next/router'
+function Post({post}){
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <h1>Loading...</h1>
+    }
+    return (
+        <>
+            <h2> {post.id} {post.title}</h2>
+            <p>{post.body}</p>
+        </>
+    )
+}
+export default Post
+
+export async function getStaticPaths(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    // const paths = data.map(post => {
+    //     return {
+    //         params:{
+    //             postId:`${post.id}`
+    //         }
+    //     }
+    // })
+    return {
+        paths:[
+            {
+                params: {postId: '1'},
+            },
+            {
+                params: {postId: '2'},
+            },
+            {
+                params: {postId: '3'},
+            },
+        ],
+        // paths,
+        fallback: 'blocking',
+    }
+}
+
+export async function getStaticProps(context){
+    const {params} = context
+    const response = await fetch (`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+    const data = await response.json()
+
+    if(!data.id) {
+        return {
+            notFound:true,
+        }
+    }
+
+    console.log(`Generating page for /post/${params.postId}`)
+
+    return {
+        props:{
+            post: data,
+        },
+    }
+}
